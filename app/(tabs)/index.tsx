3087da4c7184b45a85f1ad73c092cbb99492489d
Gimhani <silvagsd@students.nsbm.ac.lk>
2025-07-11 19:20:34 +0530

    Add trending movies section to the main index page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,6 +3,7 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { getTrendingMovies } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
 import {
@@ -16,6 +17,12 @@ import {
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: trendingMovies,
+    loading: trendingLoading,
+    error: trendingError,
+  } = useFetch(getTrendingMovies);
 
   const {
     data: movies,
@@ -32,22 +39,32 @@ export default function Index() {
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-        {moviesLoading ? (
+        {moviesLoading || trendingLoading ? (
           <ActivityIndicator
             size="large"
             color="#0000ff"
             className="mt-10 self-center"
           />
-        ) : moviesError ? (
-          <Text>Error: {moviesError}</Text>
+        ) : moviesError || trendingError ? (
+          <Text>Error: {moviesError || trendingError}</Text>
         ) : (
           <View className="flex-1 mt-5">
             <SearchBar
               onPress={() => router.push("/search")}
               placeholder="Search for movies"
-              value=""
-              onChangeText={() => {}}
+             
             />
+            { trendingMovies && (
+              <View className="mt-10">
+                <Text className="text-lg text-black font-bold mb-3">Trending Movies</Text>
+                <FlatList horizontal showsHorizontalScrollIndicator={false} ItemSeparatorComponent={() => <View className="w-4" />}
+                className="mb-4 mt-3" data={trendingMovies} renderItem={({item, index})=>(
+                  <Text className="text-black text-sm">{item.title}</Text>
+                )}
+                keyExtractor={(item) => item.movie_id.toString()}
+                />
+              </View>
+              )}
             <>
               <Text className="text-lg text-black font-bold mt-5 mb-3">
                 Latest Movies
