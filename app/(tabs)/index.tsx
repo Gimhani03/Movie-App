49589f0895a0a8cdda7e6b1bbdf93fad9e38e07b
Gimhani <silvagsd@students.nsbm.ac.lk>
2025-07-11 19:35:14 +0530

    Replace title text with TrendingCard component in trending movies list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
+import TrendingCard from "@/components/TrendingCard";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
@@ -59,7 +60,7 @@ export default function Index() {
                 <Text className="text-lg text-black font-bold mb-3">Trending Movies</Text>
                 <FlatList horizontal showsHorizontalScrollIndicator={false} ItemSeparatorComponent={() => <View className="w-4" />}
                 className="mb-4 mt-3" data={trendingMovies} renderItem={({item, index})=>(
-                  <Text className="text-black text-sm">{item.title}</Text>
+                  <TrendingCard movie={item} index={index}/>
                 )}
                 keyExtractor={(item) => item.movie_id.toString()}
                 />
