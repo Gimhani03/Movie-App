5c87a62e81c8b68aa1ed9444eb7f2d617fb11076
Gimhani <silvagsd@students.nsbm.ac.lk>
2025-07-12 16:15:29 +0530

    Add rankingGradient1 image to images export for enhanced visuals

--- constants/images.ts
+++ constants/images.ts
@@ -2,10 +2,12 @@ import bg from "@/assets/images/bg.png";
 import highlight from "@/assets/images/highlight.png";
 import highlight2 from "@/assets/images/highlight2.png";
 import rankingGradient from "@/assets/images/rankingGradient.png";
+import rankingGradient1 from "@/assets/images/rankingGradient1.png";
 
 export const images = {
   bg,
   highlight,
   highlight2,
   rankingGradient,
+  rankingGradient1,
 };
