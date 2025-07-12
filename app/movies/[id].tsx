a9b90a7c7a34697e76c25d05d192dcbca215ec0c
Gimhani <silvagsd@students.nsbm.ac.lk>
2025-07-12 17:23:05 +0530

    Refactor MovieDetails component to display detailed movie information including title, release date, runtime, rating, overview, and genres.

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,12 +1,63 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { icons } from "@/constants/icons";
+import { fetchMovieDetails } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useLocalSearchParams } from "expo-router";
+import React from "react";
+import { Image, ScrollView, View, Text } from "react-native";
+
+
+interface MovieInfoProps {
+  label: string;
+  value?: string | number | null;
+}
+const MovieInfo = ({label, value}: MovieInfoProps) => (
+  <View className="flex-col items-start justify-center mt-5">
+    <Text className="text-gray-700 font-normal text-sm">
+      {label}
+    </Text>
+    <Text className="text-gray-700 font-bold text-sm mt-2 ">
+      {value || "N/A"}
+    </Text>
+  </View>
+)
 
 const MovieDetails = () => {
-  return (
-    <View>
-      <Text>MovieDetails</Text>
-    </View>
-  )
-}
+  const {id} = useLocalSearchParams();
 
-export default MovieDetails
+  const { data: movie, loading } = useFetch(() => fetchMovieDetails(id as string));
+  return (
+    <View className="flex-1">
+      <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
+        <View>
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            style={{ width: "100%", height: 550 }}
+            resizeMode="stretch"
+          />
+          <View className="flex-col items-start justify-center mt-5 px-5">
+            <Text className="text-black font-bold text-xl">{movie?.title}</Text>
+            <View className="flex-row items-center gap-x-1 mt-2">
+              <Text className="text-sm">{movie?.release_date?.split('-')[0]}</Text>
+              <Text className="text-sm">{movie?.runtime}m</Text>
+            </View>
+            <View className="flex-row items-center bg-dark-100 py-1 rounded-md gap-x-1 mt-2">
+              <Image source={icons.star} className="size-4"/>
+              <Text className="text-black font-bold text-sm">
+                {Math.round(movie?.vote_average ?? 0)}/10
+              </Text>
+              <Text className="text-sm">
+                ({movie?.vote_count} votes)
+              </Text>
+            </View>
+            <MovieInfo label="Overview" value={movie?.overview}/>
+            <MovieInfo label="Genres" value={movie?.genres?.map((g) => g.name).join('-') || 'N/A'}/>
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default MovieDetails;
