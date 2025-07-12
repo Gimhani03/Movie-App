4da67c5055a53c39100c08013a5e6b1a07ddc360
Gimhani <silvagsd@students.nsbm.ac.lk>
2025-07-12 16:15:19 +0530

    Refactor TrendingCard component to integrate MaskedView for ranking display and enhance layout

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -1,6 +1,8 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import MaskedView from '@react-native-masked-view/masked-view';
+import { images } from '@/constants/images';
 
 const TrendingCard = ({movie:{movie_id, title, poster_url}, index}:TrendingCardProps) => {
   return (
@@ -10,7 +12,18 @@ const TrendingCard = ({movie:{movie_id, title, poster_url}, index}:TrendingCardP
             className='w-32 h-48 rounded-lg'
             resizeMode='cover' 
             />
-            <View className='absolute bottom-9 -left-3.5 px-2 py-1 rounded-full'></View>
+          
+            <View className='flex-row items-center justify-between mt-2 '>
+              <Text className='text-sm font-bold flex-1 ml-1' numberOfLines={1}>{title}</Text>
+              <View>
+                <MaskedView maskElement={
+                  <Text className='font-bold text-black text-4xl ml-7'>{index + 1}</Text>
+                }>
+                  <Image source={images.rankingGradient1} className="w-12 h-12" resizeMode="cover"/>
+                </MaskedView>
+              </View>
+            </View>
+         
         </TouchableOpacity>
     </Link>
   )
